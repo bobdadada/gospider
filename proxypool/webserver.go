26ca03700ec07b02e35065b6ab246c9b89628dfb
BAOXINGYUBOB\baoxy <baoxingyubob@outlook.com>
2022-12-26 00:23:31 +0800

    计划使用异步http请求，减少proxypool中的CPU占用过高的问题

--- proxypool/webserver.go
+++ proxypool/webserver.go
@@ -8,15 +8,15 @@ import (
 // 建立web服务，提供获取代理的功能
 func NewWebServer(s *Storage, addr string) *http.Server {
 	servermux := &http.ServeMux{}
-	servermux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	servermux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		fmt.Fprintln(w, "<h2>Welcome to Proxy Pool System</h2>")
 	})
-	servermux.HandleFunc("/random", func(w http.ResponseWriter, r *http.Request) {
+	servermux.HandleFunc("/random", func(w http.ResponseWriter, _ *http.Request) {
 		v, _ := s.Random()
 		fmt.Fprintf(w, "%v", v)
 	})
-	servermux.HandleFunc("/count", func(w http.ResponseWriter, r *http.Request) {
+	servermux.HandleFunc("/count", func(w http.ResponseWriter, _ *http.Request) {
 		v, _ := s.Count()
 		fmt.Fprintf(w, "%v", v)
 	})
